8a882de14914b7f0eacdf8b75f42ba193bd0254e
Karim-Aridj <abdelkarim.ardj@gmail.com>
2022-12-19 14:23:26 +0100

    added some configurations

--- index.js
+++ index.js
@@ -11,4 +11,11 @@ import { fileURLToPath } from "url";
 
 /** CONFIGURATIONS */
 
-const __filename = fileURLToPath(import.meta.url); // we put this configuration to grasb the file url
+const __filename = fileURLToPath(import.meta.url); // we put this configuration to grasb the file url, this is specific when we use modules,we add module we need to use this configuration to use dirct name
+const __dirname = path.dirname(__filename)
+dotenv.config(); // to invoke
+const app = express();
+app.use(express.json);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
